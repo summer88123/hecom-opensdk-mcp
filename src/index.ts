8bcab146ebda89ea759eae021b0d6cd204c5b3ae
summer88123 <summer88123@163.com>
2025-04-11 01:07:08 +0800

    feat: 更新项目配置和依赖，重构主逻辑
    更新了 `tsconfig.json` 中的 `rootDir` 为 `./src`，确保 TypeScript 编译时正确识别源文件路径。修改了 `package.json` 中的项目名称，并添加了新的依赖项和脚本。重构了 `src/index.ts`，引入了 MCP 服务器和 Hecom OpenAPI 的逻辑，实现了获取对象列表和对象描述的功能。

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,89 @@
-console.log('hello world'); // this is a comment lin
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import { z } from 'zod';
+import HClient, { ObjectMeta } from 'hecom-openapi';
+
+const server = new McpServer({
+    name: 'hecom',
+    version: '0.0.1',
+    capabilities: {
+        resources: {},
+        tools: {},
+    },
+});
+const hecom = new HClient({
+    clientId: process.env.HECOM_CLIENT_ID || '',
+    clientSecret: process.env.HECOM_CLIENT_SECRET || '',
+    username: process.env.HECOM_USERNAME || '',
+    apiHost: process.env.HECOM_HOST || '',
+});
+
+server.tool('get-objects', '获取可用的对象列表', {}, async () => {
+    const objects = await hecom.getObjects();
+
+    if (!objects || objects.length === 0) {
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: '没有可用的对象列表',
+                },
+            ],
+        };
+    }
+
+    return {
+        content: [
+            {
+                type: 'text',
+                text: JSON.stringify(
+                    objects
+                        .filter(o => o.label.length > 10)
+                        .map((obj: ObjectMeta) => ({
+                            label: obj.label,
+                            name: obj.name,
+                            description: obj.description,
+                        }))
+                ),
+            },
+        ],
+    };
+});
+
+server.tool(
+    'get-object-desc',
+    '获取对象的描述，包括对象bizType列表和field列表',
+    { label: z.string().describe(''), name: z.string().describe('对象name') },
+    async ({ name }) => {
+        const object = await hecom.getObjectDescription(name);
+
+        if (!object) {
+            return {
+                content: [
+                    {
+                        type: 'text',
+                        text: `对象 ${name} 不存在`,
+                    },
+                ],
+            };
+        }
+        return {
+            content: [
+                {
+                    type: 'text',
+                    text: JSON.stringify(object),
+                },
+            ],
+        };
+    }
+);
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error('Weather MCP Server running on stdio');
+}
+
+main().catch(error => {
+    console.error('Fatal error in main():', error);
+    process.exit(1);
+});
